0f6cb6860407f542e4c9c12f01c6050e9d26bde1
Frank Zhu <114788055+rz2606@users.noreply.github.com>
2024-03-07 16:21:53 +0800

    Add files via upload

---
+++ flake.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.utils import timezone
+
+from .file import Image
+
+# TODO: Homework: Add Retweet functionality
+# TODO: Sample solution
+
+class Flake(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(
+        'User',
+        related_name = "flakes",
+        related_query_name = "flake",
+        on_delete=models.CASCADE,
+    )
+    creation_date = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    image = models.ForeignKey(
+        Image,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    reply_to = models.ForeignKey(
+        'self',
+        related_name = "comments",
+        related_query_name = "comment",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    # retweet_of = models.ForeignKey(
+    #     'self',
+    #     related_name="retweets_of",
+    #     related_query_name="retweet_of",
+    #     on_delete=models.CASCADE,
+    #     blank=True,
+    #     null=True
+    # )
+
+    def get_likes(self):
+        return self.likes.all()
+
+    def get_comments(self):
+        return self.comments.all()
+    
+    # def retweets(self):
+    #     return Retweet.objects.filter(flake=self)
+    
+    # def get_retweets(self):
+    #     return Flake.objects.filter(retweet_of=self)
+
+    def get_retweets(self):
+        return self.retweets.all()
+
+
+class Like(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
+    creation_date = models.DateTimeField(default=timezone.now)
+    flake = models.ForeignKey(
+        Flake,
+        related_name = "likes",
+        related_query_name = "like",
+        on_delete=models.CASCADE
+    ) 
+
+    class Meta:
+        constraints = [models.UniqueConstraint("user", "flake", name="unique_like")]
+
+## Add the Retweet model
+class Retweet(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
+    creation_date = models.DateTimeField(default=timezone.now)
+    flake = models.ForeignKey(
+        Flake,
+        related_name="retweets",
+        related_query_name="retweet",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        constraints = [models.UniqueConstraint("user", "flake", name="unique_retweet")]
---
+++ user.py
@@ -0,0 +1,116 @@
+# Django Imports
+from django.db import models
+from django.contrib.auth.models import User as AuthUser
+from django.utils import timezone
+from django.db.models import Q
+
+from .file import Image
+from .flake import Flake, Like, Retweet # Import Retweet model
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    auth = models.OneToOneField(
+        AuthUser,
+        on_delete=models.CASCADE,
+    )
+    creation_date = models.DateTimeField(default=timezone.now)
+    profile_image = models.ForeignKey(
+        Image,
+        related_name='used_in_profile',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    banner_image = models.ForeignKey(
+        Image,
+        related_name='used_in_banner',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    nickname = models.CharField(max_length=100, blank=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+
+    # TODO: Slides about design pattern here, pros and cons
+    follows = models.ManyToManyField(
+        'self',
+        related_name = 'followers',
+        related_query_name = 'follower',
+        symmetrical=False,
+    )
+
+    def post_flake(self, content, image=None, reply_to=None):
+        return Flake.objects.create(
+            author = self,
+            content = content,
+            image = image,
+            reply_to = reply_to
+        )
+    
+    def delete_flake(self, flake):
+        if flake.author == self:
+            flake.delete()
+            
+## Add the retweet to the User model
+    def retweet(self, flake):
+        try:
+            Retweet.objects.get(user=self, flake=flake)
+        except Retweet.DoesNotExist:
+            Retweet.objects.create(
+                user=self,
+                flake=flake
+            )
+
+## Add the unretweet to the User model
+    def unretweet(self, flake):
+        try:
+            retweet = Retweet.objects.get(user=self, flake=flake)
+            retweet.delete()
+        except Retweet.DoesNotExist:
+            return
+
+## Modify the list_flakes method to include retweets
+    def list_flakes(self):
+        flakes = Flake.objects.filter(Q(author=self) & Q(reply_to__isnull=True))
+        retweets = Retweet.objects.filter(user=self)
+        merged = list(flakes) + list(retweets)
+        return sorted(merged, key=lambda x: x.creation_date, reverse=True)
+
+## Modify the get_feeds method to include retweets
+    def get_feeds(self):
+        flakes = Flake.objects.filter((Q(author=self) | Q(author__follower=self)) & Q(reply_to__isnull=True))
+        retweets = Retweet.objects.filter(Q(user=self) | Q(user__follower=self))
+        merged = list(flakes) + list(retweets)
+        return sorted(merged, key=lambda x: x.creation_date, reverse=True)
+
+    def like(self, flake):
+        try:
+            Like.objects.get(user=self, flake=flake)
+        except Like.DoesNotExist:
+            Like.objects.create(
+                user = self,
+                flake = flake
+            )
+    
+    def unlike(self, flake):
+        try:
+            like = Like.objects.get(user=self, flake=flake)
+            like.delete()
+        except Like.DoesNotExist:
+            return
+
+    def follow(self, followee):
+        self.follows.add(followee)
+        self.save()
+    
+    def unfollow(self, followee):
+        self.follows.remove(followee)
+        self.save()
+    
+    def get_follows(self):
+        return self.follows.all()
+    
+    def get_followers(self):
+        return self.followers.all()
+    
+
